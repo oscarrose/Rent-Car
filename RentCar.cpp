8b4fa12d7fe9d0937df13fc5db9800f186048a13
Ramon270 <67171018+Ramon270@users.noreply.github.com>
2021-12-12 10:50:52 -0400

    Menu

--- RentCar.cpp
+++ RentCar.cpp
@@ -1,9 +1,334 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
+
+
+
+
+//Funcion para manejo de coordenadas en la consola
+void gotoxy(int x, int y){
+    COORD c;
+    c.X = x; 
+    c.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+}
+
+
+
+//Menu clientes
+void menu_clientes()
+{
+	int opcionSeleccionada = 1;
+    int tecla;
+    bool menu = true;
+    
+    while(menu)
+	{
+		system("cls");
+		
+		gotoxy(5, 3 + opcionSeleccionada);
+    	cout << "==>";
+    
+	
+		gotoxy(10,2);
+    	cout<<"Menu Clientes";
+	
+		gotoxy(10,4);
+    	cout<<"1. Registrar cliente";
+ 
+    	gotoxy(10,5);
+    	cout<<"2. Consultar cliente";
+ 
+    	gotoxy(10,6);
+    	cout<<"3. Modificar cliente";
+ 
+    	gotoxy(10,7);
+    	cout<<"4. Regresar"; 
+    	
+    	tecla = getch();
+ 
+    	if (tecla == 72 && (opcionSeleccionada >=2 && opcionSeleccionada <=4))
+		{
+    	
+ 			opcionSeleccionada--;
+    	}
+    
+    	else if (tecla == 80 && (opcionSeleccionada >=1 && opcionSeleccionada <=3))
+		{
+ 
+        	opcionSeleccionada++;
+    	}
+    
+		else if (tecla == 13)
+		{
+        	if(opcionSeleccionada == 1){
+            	cout<<"Registro vehiculos";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 2){
+            	cout<<"Consultar alquiler";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 3){
+            	cout<<"Modificar alquiler";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 4){
+				menu = false;
+        	}
+    	}
+	}
+}
+
+
+//Menu vehiculos
+void menu_vehiculos()
+{
+	int opcionSeleccionada = 1;
+    int tecla;
+    bool menu = true;
+    
+    while(menu)
+	{
+		system("cls");
+		
+		gotoxy(5, 3 + opcionSeleccionada);
+    	cout << "==>";
+    
+	
+		gotoxy(10,2);
+    	cout<<"Menu Vehiculos";
+	
+		gotoxy(10,4);
+    	cout<<"1. Registrar vehiculo";
+ 
+    	gotoxy(10,5);
+    	cout<<"2. Consultar vehiculo";
+ 
+    	gotoxy(10,6);
+    	cout<<"3. Modificar vehiculo";
+ 
+    	gotoxy(10,7);
+    	cout<<"4. Iniciar Mantenimiento"; 
+    	
+    	gotoxy(10,8);
+    	cout<<"5. Terminar mantenimiento";
+		
+		gotoxy(10,9);
+    	cout<<"6. Regresar";  
+    
+    	tecla = getch();
+ 
+    	if (tecla == 72 && (opcionSeleccionada >=2 && opcionSeleccionada <=6))
+		{
+    	
+ 			opcionSeleccionada--;
+    	}
+    
+    	else if (tecla == 80 && (opcionSeleccionada >=1 && opcionSeleccionada <=5))
+		{
+ 
+        	opcionSeleccionada++;
+    	}
+    
+		else if (tecla == 13)
+		{
+        	if(opcionSeleccionada == 1){
+            	cout<<"Registro vehiculos";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 2){
+            	cout<<"Consulta vehiculo";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 3){
+            	cout<<"Modificacion vehiculo";
+            	break;
+        	}
+        	
+        	else if(opcionSeleccionada == 4){
+            	cout<<"Inicio mantenimiento";
+            	break;
+        	}
+        	
+        	else if(opcionSeleccionada == 5){
+            	cout<<"Fin mantenimiento";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 6){
+				menu = false;
+        	}
+    	}
+	}
+}
+
+
+//Menu alquileres
+void menu_alquileres()
+{
+	int opcionSeleccionada = 1;
+    int tecla;
+    bool menu = true;
+    
+    while(menu)
+	{
+		system("cls");
+		
+		gotoxy(5, 3 + opcionSeleccionada);
+    	cout << "==>";
+    
+	
+		gotoxy(10,2);
+    	cout<<"Menu Alquileres";
+	
+		gotoxy(10,4);
+    	cout<<"1. Registrar alquiler";
+ 
+    	gotoxy(10,5);
+    	cout<<"2. Consultar alquiler";
+ 
+    	gotoxy(10,6);
+    	cout<<"3. Modificar alquiler";
+ 
+    	gotoxy(10,7);
+    	cout<<"4. Cancelar alquiler"; 
+    	
+    	gotoxy(10,8);
+    	cout<<"5. Devolver vehículo";
+		
+		gotoxy(10,9);
+    	cout<<"6. Regresar";  
+    
+    	tecla = getch();
+ 
+    	if (tecla == 72 && (opcionSeleccionada >=2 && opcionSeleccionada <=6))
+		{
+    	
+ 			opcionSeleccionada--;
+    	}
+    
+    	else if (tecla == 80 && (opcionSeleccionada >=1 && opcionSeleccionada <=5))
+		{
+ 
+        	opcionSeleccionada++;
+    	}
+    
+		else if (tecla == 13)
+		{
+        	if(opcionSeleccionada == 1){
+            	cout<<"Registro vehiculos";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 2){
+            	cout<<"Consulta alquiler";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 3){
+            	cout<<"Modificacion alquiler";
+            	break;
+        	}
+        	
+        	else if(opcionSeleccionada == 4){
+            	cout<<"Cancelacion alquiler";
+            	break;
+        	}
+        	
+        	else if(opcionSeleccionada == 5){
+            	cout<<"Devolucion auto";
+            	break;
+        	}
+        
+			else if(opcionSeleccionada == 6){
+				menu = false;
+        	}
+    	}
+	}
+}
+
+
+//Menu Principal
+void menu_principal()
+{
+	int opcionSeleccionada = 1;
+    int tecla;
+    bool menu = true;
+    
+    while(menu)
+	{
+		system("cls");
+		
+		gotoxy(5, 3 + opcionSeleccionada);
+    	cout << "==>";
+    
+	
+		gotoxy(10,2);
+    	cout<<"Sistema Rent Car";
+	
+		gotoxy(10,4);
+    	cout<<"1. Gestionar clientes";
+ 
+    	gotoxy(10,5);
+    	cout<<"2. Gestionar vehiculos";
+ 
+    	gotoxy(10,6);
+    	cout<<"3. Gesstionar alquileres";
+ 
+    	gotoxy(10,7);
+    	cout<<"4. Salir\n"; 
+    
+    	tecla = getch();
+ 
+    	if (tecla == 72 && (opcionSeleccionada >=2 && opcionSeleccionada <=4))
+		{
+    	
+ 			opcionSeleccionada--;
+    	}
+    
+    	else if (tecla == 80 && (opcionSeleccionada >=1 && opcionSeleccionada <=3))
+		{
+ 
+        	opcionSeleccionada++;
+    	}
+    
+		else if (tecla == 13)
+		{
+        	if(opcionSeleccionada == 1){
+            	menu_clientes();
+        	}
+        
+			else if(opcionSeleccionada == 2){
+            	menu_vehiculos();
+        	}
+        
+			else if(opcionSeleccionada == 3){
+            	menu_alquileres();
+        	}
+        
+			else if(opcionSeleccionada == 4){
+				menu = false;
+        	}
+    	}
+	}
+}
 
 int main(int argc, char** argv) {
 	
-	
+	menu_principal();
 	
 	return 0;
 }
+
+
+
